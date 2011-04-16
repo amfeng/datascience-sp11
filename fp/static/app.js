783021585b87262a49017bdc19340d74575399e3
Amber Feng <amber.feng@gmail.com>
2011-04-15 20:50:23 -0700

    more mild styling, moved css/js to external files

---
+++ fp/static/app.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    currScrollLevel = 0;
+    
+    $("#toptagsn-iframe").hide();
+    $("#normchooser a").toggle(toNorm, toUnNorm);
+
+    function scrollFrame(left){
+        console.log("scrolling" + left);
+        //$("#toptags")animate({scrollTop: left}, 2000);
+        $("#toptags")[0].contentWindow.scrollTo(left, 0);
+        $("#toptagsn")[0].contentWindow.scrollTo(left, 0);
+        currScrollLevel = left;
+    }
+
+    $(".yearscroll").click(function() {
+        $(".yearscroll").removeClass("primary");
+        $(this).addClass("primary");
+
+        year = $(this).attr("id");
+        if(year == "ys2008"){
+            scrollFrame(0);
+        } else if (year == "ys2009") {
+            scrollFrame(950);
+        } else if (year == "ys2010") {
+            scrollFrame(2000);
+        }
+        
+    });
+
+    function toNorm() {
+        $("#toptags-iframe").hide();
+        $("#toptagsn-iframe").show();
+        $("#normchooser a span").addClass("check").addClass("icon");
+    }
+
+    function toUnNorm() {
+        $("#toptagsn-iframe").hide();
+        $("#toptags-iframe").show();
+        $("#normchooser a span").removeClass("check").removeClass("icon");
+    }
+
+    
+});
+
